1b18a24ac39d663a5533659c513bc3f974918ff3
madhavi86417 <2022wb86417@wilp.bits-pilani.ac.in>
2026-09-18 19:07:20 +0530

    Update greeting message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
  
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, DevOps Continuous Integration Lab!");
+        System.out.println("Hello,Continuous Integration triggered Successfully!");
     }
 }
