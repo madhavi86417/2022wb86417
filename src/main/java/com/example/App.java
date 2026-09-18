384ac50d032e122e6815150deb73778fc5e20a19
madhavi86417 <2022wb86417@wilp.bits-pilani.ac.in>
2026-09-18 19:16:33 +0530

    Remove 'Hello,' from output message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
  
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello,Continuous Integration triggered Successfully!");
+        System.out.println("Continuous Integration triggered Successfully!");
     }
 }
